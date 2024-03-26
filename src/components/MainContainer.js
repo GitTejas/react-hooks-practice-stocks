ad294587b79e1f636416a1bfd1848453f6115a23
Tejas Nampalli <TSNampalli@gmail.com>
2024-03-26 16:35:01 -0400

    finished

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,18 +1,58 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
 function MainContainer() {
+
+const [stocks, setStocks] = useState([])
+const [myStocks, setMyStocks] = useState([])
+const [sortBy, setSortBy] = useState("")
+const [filterBy, setFilterBy] = useState("")
+
+useEffect(() => {
+  fetch('http://localhost:3001/stocks')
+  .then(resp => resp.json())
+  .then(setStocks)
+}, [])
+
+function handleAddStock(stockToAdd) {
+  setMyStocks(currentMyStocks => [...currentMyStocks, stockToAdd])
+}
+
+function handleRemoveStock(stockToRemove) {
+ setMyStocks(currentMyStocks => currentMyStocks.filter(stock => stock.id !== stockToRemove.id) )
+}
+
+function handleToggleSort(e) {
+  setSortBy(e.target.value)
+}
+
+function handleFilter(e) {
+  setFilterBy(e.target.value)
+}
+
   return (
     <div>
-      <SearchBar />
+      <SearchBar 
+      sortBy={sortBy}
+      handleToggleSort={handleToggleSort}
+      handleFilter={handleFilter}
+      filterBy={filterBy} />
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer 
+          stocks={stocks}
+          handleAddStock={handleAddStock}
+          sortBy={sortBy}
+          filterBy={filterBy}
+           />
         </div>
         <div className="col-4">
-          <PortfolioContainer />
+          <PortfolioContainer 
+          myStocks={myStocks}
+          handleRemoveStock={handleRemoveStock} 
+          />
         </div>
       </div>
     </div>
--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,12 +1,17 @@
 import React from "react";
 import Stock from "./Stock";
 
-function PortfolioContainer() {
+function PortfolioContainer({myStocks, handleRemoveStock}) {
   return (
     <div>
       <h2>My Portfolio</h2>
       {
-        //render your portfolio stocks here
+        myStocks.map(stock => 
+        <Stock 
+        {...stock} 
+        key={stock.id}
+        handleStock={handleRemoveStock} 
+        />)
       }
     </div>
   );
